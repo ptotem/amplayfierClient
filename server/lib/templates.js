9a8818211641dac75f159c6d1e8d53b4831a4f31
rakesh19487 <rakesh.rengaraj@gmail.com>
2015-04-16 12:43:34 +0530

    moved to ptotem.in

--- server/lib/templates.js
+++ server/lib/templates.js
@@ -1,8 +1,8 @@
 Templates = {};
 
 Templates.sampleMail = {
-    path: '/var/www/assets/sample-email/template.html',    // Relative to the 'private' dir.
-    scss: '/var/www/assets/sample-email/style.scss',       // Mail specific SCSS.
+    path: '/assets/sample-email/template.html',    // Relative to the 'private' dir.
+    scss: '/assets/sample-email/style.scss',       // Mail specific SCSS.
 
     helpers: {
         capitalizedName: function() {
@@ -22,8 +22,8 @@ Templates.sampleMail = {
     }
 };
 Templates.resetPassword = {
-    path: '/var/www/assets/sample-email/resetpassword.html',    // Relative to the 'private' dir.
-    scss: '/var/www/assets/sample-email/style.scss',       // Mail specific SCSS.
+    path: '/assets/sample-email/resetpassword.html',    // Relative to the 'private' dir.
+    scss: '/assets/sample-email/style.scss',       // Mail specific SCSS.
 
     helpers: {
         capitalizedName: function() {
@@ -44,7 +44,7 @@ Templates.resetPassword = {
 };
 
 Templates.newRegister = {
-    path: '/var/www/assets/sample-email/newuser.html',    // Relative to the 'private' dir.
+    path: '/assets/sample-email/newuser.html',    // Relative to the 'private' dir.
 
 
     helpers: {
@@ -67,7 +67,7 @@ Templates.newRegister = {
 
 
 Templates.assessmentMail = {
-    path: '/var/www/assets/sample-email/assessmentMail.html',    // Relative to the 'private' dir.
+    path: '/assets/sample-email/assessmentMail.html',    // Relative to the 'private' dir.
 
 
     helpers: {
