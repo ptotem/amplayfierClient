570cc406fb43c50f1255515c234b1a99e90fbdba
rakesh19487 <rakesh.rengaraj@gmail.com>
2015-04-16 12:09:37 +0530

    again path changed

--- server/lib/templates.js
+++ server/lib/templates.js
@@ -1,7 +1,7 @@
 Templates = {};
 
 Templates.sampleMail = {
-    path: '/var/www/templates/sample-email/template.html',    // Relative to the 'private' dir.
+    path: '/assets/sample-email/template.html',    // Relative to the 'private' dir.
     scss: 'sample-email/style.scss',       // Mail specific SCSS.
 
     helpers: {
@@ -22,7 +22,7 @@ Templates.sampleMail = {
     }
 };
 Templates.resetPassword = {
-    path: '/var/www/templates/sample-email/resetpassword.html',    // Relative to the 'private' dir.
+    path: '/assets/sample-email/resetpassword.html',    // Relative to the 'private' dir.
     scss: 'sample-email/style.scss',       // Mail specific SCSS.
 
     helpers: {
@@ -44,7 +44,7 @@ Templates.resetPassword = {
 };
 
 Templates.newRegister = {
-    path: '/var/www/templates/sample-email/newuser.html',    // Relative to the 'private' dir.
+    path: '/assets/sample-email/newuser.html',    // Relative to the 'private' dir.
 
 
     helpers: {
@@ -67,7 +67,7 @@ Templates.newRegister = {
 
 
 Templates.assessmentMail = {
-    path: '/var/www/templates/sample-email/assessmentMail.html',    // Relative to the 'private' dir.
+    path: '/assets/sample-email/assessmentMail.html',    // Relative to the 'private' dir.
 
 
     helpers: {
