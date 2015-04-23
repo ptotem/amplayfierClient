c5d5365b0a1cc03b415874a1fe917781fe7250c2
The Dark Lord <rushabhhathi@gmail.com>
2015-04-24 01:52:03 +0530

    full screen issue sokved

--- client/lib/js/story-adaptor.js
+++ client/lib/js/story-adaptor.js
@@ -596,8 +596,16 @@ function cancelFullScreen(elm) {
     $('.modal-xlg-ppt').css({"height":"82%"});
 
     //done for demo
-    $($('.slide-container').find('iframe')[0].contentWindow.document).find('embed').css({"transform":"scale(1.0)"});
+    //$($('.slide-container').find('iframe')[0].contentWindow.document).find('embed').css({"transform":"scale(1.0)"});
+    setTimeout(function(){
+            $('.slide-container').find('iframe').css({"transform":"scale(1.0) translateY(0%)"});
+            $('.slide-container').find('iframe').css({"margin-top":""});
+        }
+
+        ,3000);
+
     $('.modal-xlg-ppt').css({"height":"72%"});
+    $('.full-container').css({'margin-top':'4.9%'});
 
 
 
@@ -622,16 +630,20 @@ function requestFullScreen(elm) {
     //$('.main-story-content').hide();
     $(elm).fadeIn();
     $('.show-deck-modal').css({"height":"120%"});
-    $('.slide-container').css({"transform":"scale(1.1) translateX(7.3%)"});
-    $('.slide-container').find('iframe').css({"margin-top":"7%"});
+    //$('.slide-container').css({"transform":"scale(1.1) translateX(7.3%)"});
+    //$('.slide-container').find('iframe').css({"margin-top":"7%"});
 
     //only done for demo on 24 april 2015
-    $('.slide-container').find('iframe').css({"margin-top":"0%"});
+    //$('.slide-container').find('iframe').css({"margin-top":"0%"});
     //$('.slide-container').find('iframe').find('embed').css({"transform":"scale(1.4)"});
-    $($('.slide-container').find('iframe')[0].contentWindow.document).find('embed').css({"transform":"scale(1.1)"});
+    setTimeout(function(){
+            $('.slide-container').find('iframe').css({"transform":"scale(1.15) translateY(7%)"});
+    }
+    ,3000);
+
     //change 65 to 74..done for demo
     $('.modal-xlg-ppt').css({"height":"65%"});
-    $('.slide-container').css({"transform":"scale(1.1) translateX(4%)"});
+    //$('.slide-container').css({"transform":"scale(1.1) translateX(4%)"});
     $('.full-container').css({'margin-top':'6.2%'});
     //$('.component').css({"transform":"scale(1.4) "});
 
