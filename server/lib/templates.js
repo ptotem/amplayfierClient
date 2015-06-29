eecd04b476b59a84ee606ba4f56916a97f7eb67c
rakesh19487 <rakesh.rengaraj@gmail.com>
2015-06-29 22:58:51 +0530

    sample template.js commented

--- server/lib/templates.js
+++ server/lib/templates.js
@@ -1,90 +1,89 @@
-Templates = {};
-
-Templates.sampleMail = {
-    path: 'sample-email/template.html',    // Relative to the 'private' dir.
-    scss: 'sample-email/style.scss',       // Mail specific SCSS.
-
-    helpers: {
-        capitalizedName: function() {
-            return this.name.charAt(0).toUpperCase() + this.name.slice(1);
-        }
-    },
-
-    route: {
-        path: '/sample/:name',
-
-        data: function() {
-            return {
-                name: this.params.name,
-                names: ['Johan', 'John', 'Paul', 'Ringo']
-            };
-        }
-    }
-};
-Templates.resetPassword = {
-    path: 'sample-email/resetpassword.html',    // Relative to the 'private' dir.
-    scss: 'sample-email/style.scss',       // Mail specific SCSS.
-
-    helpers: {
-        capitalizedName: function() {
-            return this.name.charAt(0).toUpperCase() + this.name.slice(1);
-        }
-    },
-
-    route: {
-        path: '/resetpassword/:name',
-
-        data: function() {
-            return {
-                name: this.params.name,
-                names: ['Johan', 'John', 'Paul', 'Ringo']
-            };
-        }
-    }
-};
-
-Templates.newRegister = {
-    path: 'sample-email/newuser.html',    // Relative to the 'private' dir.
-
-
-    helpers: {
-        capitalizedName: function() {
-            return this.name.charAt(0).toUpperCase() + this.name.slice(1);
-        }
-    },
-
-    route: {
-        path: '/newuser/:name',
-
-        data: function() {
-            return {
-                name: this.params.name,
-                names: ['Johan', 'John', 'Paul', 'Ringo']
-            };
-        }
-    }
-};
-
-
-Templates.assessmentMail = {
-    path: 'sample-email/assessmentMail.html',    // Relative to the 'private' dir.
-
-
-    helpers: {
-        capitalizedName: function() {
-            return this.name.charAt(0).toUpperCase() + this.name.slice(1);
-        }
-    },
-
-    route: {
-        path: '/assessmentMail/:name',
-
-        data: function() {
-            return {
-                name: this.params.name,
-                names: ['Johan', 'John', 'Paul', 'Ringo']
-            };
-        }
-    }
-};
-
+// Templates = {};
+//
+// Templates.sampleMail = {
+//     path: 'sample-email/template.html',    // Relative to the 'private' dir.
+//     scss: 'sample-email/style.scss',       // Mail specific SCSS.
+//
+//     helpers: {
+//         capitalizedName: function() {
+//             return this.name.charAt(0).toUpperCase() + this.name.slice(1);
+//         }
+//     },
+//
+//     route: {
+//         path: '/sample/:name',
+//
+//         data: function() {
+//             return {
+//                 name: this.params.name,
+//                 names: ['Johan', 'John', 'Paul', 'Ringo']
+//             };
+//         }
+//     }
+// };
+// Templates.resetPassword = {
+//     path: 'sample-email/resetpassword.html',    // Relative to the 'private' dir.
+//     scss: 'sample-email/style.scss',       // Mail specific SCSS.
+//
+//     helpers: {
+//         capitalizedName: function() {
+//             return this.name.charAt(0).toUpperCase() + this.name.slice(1);
+//         }
+//     },
+//
+//     route: {
+//         path: '/resetpassword/:name',
+//
+//         data: function() {
+//             return {
+//                 name: this.params.name,
+//                 names: ['Johan', 'John', 'Paul', 'Ringo']
+//             };
+//         }
+//     }
+// };
+//
+// Templates.newRegister = {
+//     path: 'sample-email/newuser.html',    // Relative to the 'private' dir.
+//
+//
+//     helpers: {
+//         capitalizedName: function() {
+//             return this.name.charAt(0).toUpperCase() + this.name.slice(1);
+//         }
+//     },
+//
+//     route: {
+//         path: '/newuser/:name',
+//
+//         data: function() {
+//             return {
+//                 name: this.params.name,
+//                 names: ['Johan', 'John', 'Paul', 'Ringo']
+//             };
+//         }
+//     }
+// };
+//
+//
+// Templates.assessmentMail = {
+//     path: 'sample-email/assessmentMail.html',    // Relative to the 'private' dir.
+//
+//
+//     helpers: {
+//         capitalizedName: function() {
+//             return this.name.charAt(0).toUpperCase() + this.name.slice(1);
+//         }
+//     },
+//
+//     route: {
+//         path: '/assessmentMail/:name',
+//
+//         data: function() {
+//             return {
+//                 name: this.params.name,
+//                 names: ['Johan', 'John', 'Paul', 'Ringo']
+//             };
+//         }
+//     }
+// };
